8fa5003b6ec2faececdd86f0b8cfd986ac86028c
EleMint <dylanschmidt0216@gmail.com>
2018-09-13 13:48:57 -0500

    noStroke

--- sketch.js
+++ sketch.js
@@ -1,17 +1,17 @@
 var canvas;
 
-function setup(){
+function setup() {
     canvas = createCanvas(window.innerWidth, window.innerHeight);
-
 }
 
-function draw(){
+function draw() {
     background(51);
+    noStroke();
     fill(255);
-    ellipse(width/2, height/2, 100, 100);
+    ellipse(width / 2, height / 2, 100, 100);
 }
 
-window.onresize = function(){
+window.onresize = function () {
     var w = window.innerWidth;
     var h = window.innerHeight;
     canvas.size(w, h);
